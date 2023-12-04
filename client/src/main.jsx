a6e13361cfaa7b5e4521a59849790171145dda3e
tuan23590 <thanhtuan.htcv@gmail.com>
2023-12-04 19:19:55 +0700

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
+import App from './App.jsx'
 import './index.css'
-import router from './router';
-import { Container } from '@mui/material'
-import './firebase/config';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Container maxWidth='lg' sx={{textAlign:"center",marginTop:'50px'}}>
-    <RouterProvider router={router} />
-    </Container>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
