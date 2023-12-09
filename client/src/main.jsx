c9cace538a964e9ebad438f5282edaf3f91673fb
tuan3110 <chodai23590@gmail.com>
2023-12-09 22:50:08 +0700

    .

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,8 +7,8 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./index.css";
-import "./firebase/config";
-import router from "./router";
+import "./firebase/config.jsx";
+import router from "./router/index.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
       <RouterProvider router={router} />
