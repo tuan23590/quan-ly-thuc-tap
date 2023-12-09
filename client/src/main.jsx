ffd27a3f28800ca318235a14a249b4cc98a25467
tuan3110 <chodai23590@gmail.com>
2023-12-09 23:19:58 +0700

    check error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,24 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { RouterProvider } from "react-router-dom";
+
+
+// import "@fontsource/roboto/300.css";
+// import "@fontsource/roboto/400.css";
+// import "@fontsource/roboto/500.css";
+// import "@fontsource/roboto/700.css";
+// import "./index.css";
+// import router from "./router";
+// import "./firebase/config";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//       <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { RouterProvider } from "react-router-dom";
-
-
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -11,8 +27,14 @@ import "./index.css";
 import router from "./router";
 import "./firebase/config";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
       <RouterProvider router={router} />
-  </React.StrictMode>
-);
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element with id 'root' not found");
+}
