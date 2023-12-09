84252712735bc29f2d5fedbc7ba02cfbff848609
tuan3110 <chodai23590@gmail.com>
2023-12-09 23:15:14 +0700

    ...

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
+
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./index.css";
-import "./firebase/config.jsx";
-import router from "./router/index.jsx";
+import router from "./router";
+import "./firebase/config";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
       <RouterProvider router={router} />
