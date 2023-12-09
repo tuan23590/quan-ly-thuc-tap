58407380e5264e6ae61aec601aa6eecdb2be7fc5
tuan3110 <chodai23590@gmail.com>
2023-12-09 22:19:09 +0700

    .

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {build: {
-    // generate .vite/manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: 'src/main.jsx',
-    },
-  },
+  optimizeDeps: {
     include: [
       '@emotion/react', 
       '@emotion/styled', 
