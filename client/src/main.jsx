7ec9145366102f02969c141eba6a821dc406a228
tuan3110 <chodai23590@gmail.com>
2023-12-10 11:20:32 +0700

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,8 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { RouterProvider } from "react-router-dom";
-
-
-// import "@fontsource/roboto/300.css";
-// import "@fontsource/roboto/400.css";
-// import "@fontsource/roboto/500.css";
-// import "@fontsource/roboto/700.css";
-// import "./index.css";
-// import router from "./router";
-// import "./firebase/config";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//       <RouterProvider router={router} />
-//   </React.StrictMode>
-// );
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+
+
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -27,14 +11,8 @@ import "./index.css";
 import router from "./router";
 import "./firebase/config";
 
-const rootElement = document.getElementById("root");
-
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
       <RouterProvider router={router} />
-    </React.StrictMode>
-  );
-} else {
-  console.error("Root element with id 'root' not found");
-}
+  </React.StrictMode>
+);
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  optimizeDeps: {
-    include: [
-      '@emotion/react', 
-      '@emotion/styled', 
-      '@mui/material/Tooltip'
-    ],
-  },
+ 
   plugins: [react()],
 })
