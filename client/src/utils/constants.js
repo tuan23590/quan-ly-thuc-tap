beaf8ce1b336a0cde279f7a5afab058d3dc642c7
tuan3110 <chodai23590@gmail.com>
2023-12-27 05:43:26 +0700

    pdate

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const GRAPHQL_SERVER = 'http://localhost:4000/';
+export const GRAPHQL_SERVER = 'http://localhost:4000/'
 //export const GRAPHQL_SERVER = 'https://quan-ly-thuc-tap-server.onrender.com';
